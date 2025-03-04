6e92db35861c3d37e96a0d19cc9b4ce561fbe4e5
violet <lvhoa58@gmail.com>
2025-03-04 12:11:53 +0700

    configure hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,60 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-abi-exporter";
+import { task } from "hardhat/config";
+import * as dotenv from "dotenv";
+import "@openzeppelin/hardhat-upgrades";
+
+dotenv.config();
+
+const {
+  TESTNET_PRIVATE_KEY: testnetPrivateKey,
+  MAINNET_PRIVATE_KEY: mainnetPrivateKey,
+} = process.env;
+
+task("accounts", "Prints the list of accounts", async (_taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  networks: {
+    bscTestnet: {
+      url: "https://bsc-testnet-rpc.publicnode.com",
+      chainId: 97,
+      accounts: [testnetPrivateKey!],
+    },
+    berachain: {
+      url: "https://rpc.berachain.com",
+      chainId: 80094,
+      accounts: [mainnetPrivateKey!],
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.28",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+          evmVersion: "cancun",
+        },
+      },
+    ],
+  },
+  abiExporter: {
+    path: "data/abi",
+    runOnCompile: true,
+    clear: true,
+    flat: false,
+    only: [],
+    spacing: 4,
+  },
 };
 
 export default config;
